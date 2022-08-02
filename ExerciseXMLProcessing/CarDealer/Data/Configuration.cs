e250c5321ffc29f169908ae9ea1447452888e9e7
Stoyan <stoqnd1999@gmail.com>
2022-08-02 09:29:32 +0300

    XML

---
+++ ExerciseXMLProcessing/CarDealer/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=STOYAN\SQLEXPRESS;Database=CarDealer;Trusted_Connection=True;";
+    }
+}
